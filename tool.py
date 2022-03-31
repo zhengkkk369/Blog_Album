764bc1ba24c53308683f5b6ba921b11ff2c6d5e4
汶晓 郑 <2442292187@qq.com>
2022-03-31 18:24:59 +0800

    add photos

--- tool.py
+++ tool.py
@@ -209,7 +209,7 @@ def handle_photo():
             list_info[-1]['arr']['type'].append('image')
     list_info.reverse()  # 翻转
     final_dict = {"list": list_info}
-    with open("E:/malizhiBlog/blog/themes/next/source/lib/album/data.json","w") as fp:
+    with open("D:/Blog/themes/next/source/lib/album/data.json","w") as fp:
         json.dump(final_dict, fp)
 def cut_photo():
     """裁剪算法
